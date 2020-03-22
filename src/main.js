b985ddf62a966e2cd2d899ad2d80a98f50309243
Mikhail Gaus <mikhail.gaus@ges.thm.de>
2020-03-22 10:24:46 +0100

    added Apollo

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import 'carbon-components/css/carbon-components.css';
 import CarbonComponentsVue from '@carbon/vue/src/index';
 import Vue from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(CarbonComponentsVue);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  apolloProvider: createProvider(),
+  render: h => h(App)
+}).$mount('#app');
